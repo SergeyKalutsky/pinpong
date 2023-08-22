092d9c6bbb1979a11acd4a21316fb320cedea4a8
SergeyKalutsky <skalutsky@gmail.com>
2023-08-22 17:27:05 +0300

    added bar

--- main.py
+++ main.py
@@ -28,25 +28,26 @@ class GameSprite(pg.sprite.Sprite):
 class Pokemon(GameSprite):
     def __init__(self, image, x, y):
         super().__init__(image, x, y)
-        self.speed = 5
+        self.speed = 3
 
     def update(self):
         self.rect.x += self.speed
         if self.speed > 0 and self.rect.x > WIDTH - 350:
             self.speed *= -1
-        if self.speed < 0 and self.rect.x < 150:
+        if self.speed < 0 and self.rect.x < 200:
             self.speed *= -1
 
 
 class Arrow():
-    def __init__(self, w, h, x, y):
+    def __init__(self, w, h, x, y, color):
         self.rect = pg.Rect(w, h, x, y)
         self.speed = 5
         self.count = 0
         self.side = True
+        self.color = color
 
     def draw(self):
-        pg.draw.rect(screen, 'orange', self.rect)
+        pg.draw.rect(screen, self.color, self.rect)
 
     def update(self):
 
@@ -66,8 +67,8 @@ ball = GameSprite('ball.png', 450, 450)
 pokemons = os.listdir("pokemons")
 pokemon = Pokemon(f'pokemons\{choice(pokemons)}', 450, 50)
 
-# aqua_arrow = pg.draw.rect(screen, 'aqua', (50, 50, 20, 500))
-player_arrow = Arrow(20, 20, 50, 50)
+aqua_arrow = Arrow(100, 50, 40, 500, 'aqua')
+player_arrow = Arrow(100, 50, 40, 70, 'orange')
 
 
 while True:
@@ -77,7 +78,7 @@ while True:
             sys.exit()
 
     ball.draw()
-    pg.draw.rect(screen, 'aqua', (50, 50, 20, 500))
+    aqua_arrow.draw()
 
     pokemon.update()
     pokemon.draw()
